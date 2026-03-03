7d49109a50ed106cc27ee527382094d4716f2c39
Dhanusha-19 <vdhanushaa.19@gmail.com>
2026-03-03 10:47:51 +0530

    new feature uc4

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,39 +2,17 @@ public class OOPSBanner {
 
     public static void main(String[] args) {
 
-        String line1 = String.join("   ",
-                " OOOO ",
-                " OOOO ",
-                " PPPP ",
-                " SSSS ");
-        System.out.println(line1);
+        String[] banner = {
 
-        String line2 = String.join("   ",
-                "O    O",
-                "O    O",
-                "P    P",
-                "S     ");
-        System.out.println(line2);
+            String.join("   ", " ***** ", " ***** ", " ***** ", " ***** "),
+            String.join("   ", "*     *", "*     *", "*     *", "*      "),
+            String.join("   ", "*     *", "*     *", " ***** ", " ***** "),
+            String.join("   ", "*     *", "*     *", "*      ", "      *"),
+            String.join("   ", " ***** ", " ***** ", "*      ", " ***** ")
+        };
 
-        String line3 = String.join("   ",
-                "O    O",
-                "O    O",
-                "PPPPP ",
-                " SSSS ");
-        System.out.println(line3);
-
-        String line4 = String.join("   ",
-                "O    O",
-                "O    O",
-                "P     ",
-                "     S");
-        System.out.println(line4);
-
-        String line5 = String.join("   ",
-                " OOOO ",
-                " OOOO ",
-                "P     ",
-                " SSSS ");
-        System.out.println(line5);
+        for (int i = 0; i < banner.length; i++) {
+            System.out.println(banner[i]);
+        }
     }
 }
